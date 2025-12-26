f43f0297d38823c215cf8d55598547e5592356b3
siyer20 <90734516+siyer20@users.noreply.github.com>
2025-12-26 13:11:25 -0500

    Completed Exercise 7 in foundations

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,4 +1,9 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let newStr = '';
+    for (i=word.length-1; i>=0; i--){
+        newStr += word[i];
+    }
+    return newStr
 
 };
 
