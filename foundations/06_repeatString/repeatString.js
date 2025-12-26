7378aace4fa26c925ec1694b7c802d4d8f33ba84
siyer20 <90734516+siyer20@users.noreply.github.com>
2025-12-26 13:04:33 -0500

    Completed Exercise 6

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    let counter = 0;
+    let newStr='';
+    if (num < 0){
+        return 'ERROR'
+    }
+    while (counter < num){
+        newStr += word;
+        counter++;
+    }
+    return newStr
 };
 
 // Do not edit below this line
