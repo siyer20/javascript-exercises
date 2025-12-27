540116eeb062e456f05c7fa59ace73c7938d31a6
siyer20 <90734516+siyer20@users.noreply.github.com>
2025-12-26 22:21:30 -0500

    Completed Exercise 10 in foundations

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,4 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if(year%4==0){
+        console.log(year + "is divisible by 4");
+        if(year%100 == 0 && year%400 != 0){
+            console.log(year + "is divisible by 100 but not 400");
+            return false
+        }else{
+            console.log("year is divisible by 4!")
+            return true
+        }
+    }
+    return false
 
 };
 
