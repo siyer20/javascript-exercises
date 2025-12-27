432c34496bdcf2b5a62a690dc4b16271845434e1
siyer20 <90734516+siyer20@users.noreply.github.com>
2025-12-26 22:14:14 -0500

    Completed Exercise 9 in foundations

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (Number.isInteger(min) && Number.isInteger(max) && min>=0 && max>=0){
+        let sum = 0;
 
+        for(i=Math.min(min, max); i<=Math.max(min, max); i++){
+            sum+=i;
+        }
+
+        return sum
+    }
+
+    return 'ERROR'
 };
 
 // Do not edit below this line
